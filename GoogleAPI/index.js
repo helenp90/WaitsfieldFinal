d9bda091d1c40560f25a2360524d7a190b5f940c
Helen Pitt <helenpitt1990@gmail.com>
2018-10-08 19:35:46 -0400

    adding the mail js to the index.js file

--- GoogleAPI/index.js
+++ GoogleAPI/index.js
@@ -85,3 +85,23 @@ function listLabels(auth) {
     }
   });
 }
+/**
+ * Insert Message into user's mailbox.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {String} email RFC 5322 formatted String.
+ * @param  {Function} callback Function to call when the request is complete.
+ */
+function insertMessage(userId, email, callback) {
+  // Using the js-base64 library for encoding:
+  // https://www.npmjs.com/package/js-base64
+  var base64EncodedEmail = Base64.encodeURI(email);
+  var request = gapi.client.gmail.users.messages.insert({
+    'userId': userId,
+    'resource': {
+      'raw': base64EncodedEmail
+    }
+  });
+  request.execute(callback);
+}
