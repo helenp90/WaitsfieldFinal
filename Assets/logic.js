3f95821121eaff5dd6c7e838006e79d786086255
Helen Pitt <helenpitt1990@gmail.com>
2018-10-08 18:36:54 -0400

    with fancy new graphics

---
+++ Assets/logic.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $('.parallax').parallax();
+//   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
